cbf585f3e2050f8f8ffd451284f4e8e6e56d813e
Christian0428 <cpurog2535@sdsu.edu>
2025-10-27 15:35:28 -0700

    Finished upHead and installed push

--- heap.h
+++ heap.h
@@ -16,6 +16,9 @@ struct MinHeap {
 
     void push(int idx, int weightArr[]) {
         // TODO: insert index at end of heap, restore order using upheap()
+        data[size] = idx; //adds the int idx to teh array
+        upheap(size, weightArr); //calls upheap to check if any reorganizing needs to be done
+        size++; //incriments the size for proper position use
     }
 
     int pop(int weightArr[]) {
@@ -26,6 +29,15 @@ struct MinHeap {
 
     void upheap(int pos, int weightArr[]) {
         // TODO: swap child upward while smaller than parent
+
+        while (pos > 0) { //continously runs until explicitly broken
+            int parent = (pos - 1) /2; //initializes parent position
+            if (data[parent] < data[pos]) { //data instead of weightArr because we need the index of the dataArr instead of the weightArr
+                swap(data[pos], data[parent]); //ofc swaps the values of pos and parent
+                pos = parent; //shifts the values
+            }
+            else break; //breaks whenever the parent is greater than the position
+        }
     }
 
     void downheap(int pos, int weightArr[]) {
